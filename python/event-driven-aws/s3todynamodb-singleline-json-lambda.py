9dc224e132ffd4997de9f7c3e2dd54089cbf3e23
devops-pritam <132892500+devops-pritam@users.noreply.github.com>
2024-03-31 16:22:33 +0100

    Create s3todynamodb-singleline-json-lambda.py

---
+++ python/event-driven-aws/s3todynamodb-singleline-json-lambda.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+import ast
+
+def lambda_handler(event, context):
+    # TODO implement
+    object_key = event['Records'][0]['s3']['object']['key']
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    print(f"bucket name: {bucket_name} and object name: {object_key}")
+    s3 = boto3.client('s3', region_name='ap-south-1')
+    response = s3.get_object(Bucket = bucket_name, Key = object_key)
+    file_reader = response['Body'].read().decode("utf-8")
+    print(type(file_reader))
+    file_reader2 = ast.literal_eval(file_reader)
+    print(type(file_reader2))
+    print(file_reader2)
+    dynamodb_client = boto3.resource('dynamodb')
+    table = dynamodb_client.Table('event-table')
+    table.put_item(Item=file_reader2)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
